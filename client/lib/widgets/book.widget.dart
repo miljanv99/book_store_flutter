440e9ffad96553b2bb2a2762ea00a71462f7d39b
miljanv99 <miljanvukovic85@gmail.com>
2023-10-25 21:27:11 +0200

    small ui changes for book widget

--- client/lib/widgets/book.widget.dart
+++ client/lib/widgets/book.widget.dart
@@ -11,49 +11,54 @@ class BookCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 250,
-      height: 300,
+      height: 350,
       child: Card(
-      elevation: 4, // Adjust the shadow of the card
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0), // Set border radius
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
-            child: Image.network(
-              imageUrl,
-              fit: BoxFit.fill,
-              width: 250,
-              height: 200, // Set the desired height for the image
+        elevation: 4, // Adjust the shadow of the card
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0), // Set border radius
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.fill,
+                width: 250,
+                height: 220,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                            overflow: TextOverflow.ellipsis),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        author,
+                        style: TextStyle(fontSize: 14.0, color: Colors.grey),
+                      ),
+                      SizedBox(height: 4.0),
+                      ElevatedButton(
+                          style: ButtonStyle(),
+                          onPressed: () => {print(title)},
+                          child: Text('Add to cart'))
+                    ],
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 4.0),
-                Text(
-                  author,
-                  style: TextStyle(fontSize: 14.0, color: Colors.grey),
-                ),
-              ],
-            ),
-          ),
-        ],
+                )),
+          ],
+        ),
       ),
-    ),
     );
   }
 }

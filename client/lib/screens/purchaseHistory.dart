93970d2478cbf5e1d766371ba6e33bd38c165050
miljanv99 <miljanvukovic85@gmail.com>
2024-02-08 23:30:21 +0100

    add empty purchase history

--- client/lib/screens/purchaseHistory.dart
+++ client/lib/screens/purchaseHistory.dart
@@ -29,87 +29,92 @@ class _PurchaseHistoryState extends State<PurchaseHistory> {
           child: Container(
             width: maxWidth,
             child: Consumer<AuthorizationProvider>(
-            builder: (context, authNotifier, child) {
-              String token = authNotifier.token;
-              receipts = authNotifier.updatePurchaseHistory(token);
-              return FutureBuilder(
-                future: receipts,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot.error}'),
-                    );
-                  } else {
-                    List<Receipt> receiptList = snapshot.data!;
+              builder: (context, authNotifier, child) {
+                String token = authNotifier.token;
+                receipts = authNotifier.updatePurchaseHistory(token);
+                return FutureBuilder(
+                  future: receipts,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.data!.isEmpty) {
+                      return const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.history_rounded),
+                          SizedBox(height: 10),
+                          Text('No purchase history')
+                        ],
+                      );
+                    } else {
+                      List<Receipt> receiptList = snapshot.data!;
 
-                    return ListView.builder(
-                      itemCount: receiptList.length,
-                      itemBuilder: (context, index) {
-                        Receipt receipt = receiptList[index];
-                        List<Book> books = receipt.productsInfo;
+                      return ListView.builder(
+                        itemCount: receiptList.length,
+                        itemBuilder: (context, index) {
+                          Receipt receipt = receiptList[index];
+                          List<Book> books = receipt.productsInfo;
 
-                        return Card(
-                          elevation: 5,
-                          margin: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ListTile(
-                                  title: Text(
-                                    'Receipt ID: ${receipt.id}',
-                                    style: const TextStyle(fontSize: 15),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                  subtitle: Center(
-                                    child: Text(
-                                      'Total Price: \$${receipt.totalPrice.toStringAsFixed(2)}',
-                                      style: const TextStyle(
-                                          color: Colors.blueAccent),
-                                    ),
-                                  )),
-                              const Divider(),
-                              ListView.builder(
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                itemCount: books.length,
-                                itemBuilder: (context, bookIndex) {
-                                  Book book = books[bookIndex];
-                                  return ListTile(
-                                    leading: Image.network(
-                                      book.cover ?? '',
-                                      width: 60,
-                                      height: 80,
-                                    ),
+                          return Card(
+                            elevation: 5,
+                            margin: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ListTile(
                                     title: Text(
-                                      '${book.title}',
-                                      style: const TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.blueAccent),
+                                      'Receipt ID: ${receipt.id}',
+                                      style: const TextStyle(fontSize: 15),
                                       textAlign: TextAlign.center,
                                     ),
-                                    subtitle: Column(
-                                      children: [
-                                        Text(
-                                            'Quantity: ${book.price} x ${book.qty}'),
-                                        Text('Author: ${book.author}')
-                                      ],
-                                    ),
-                                  );
-                                },
-                              ),
-                              const SizedBox(height: 8.0),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  }
-                },
-              );
-            },
-          ),
+                                    subtitle: Center(
+                                      child: Text(
+                                        'Total Price: \$${receipt.totalPrice.toStringAsFixed(2)}',
+                                        style: const TextStyle(
+                                            color: Colors.blueAccent),
+                                      ),
+                                    )),
+                                const Divider(),
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  itemCount: books.length,
+                                  itemBuilder: (context, bookIndex) {
+                                    Book book = books[bookIndex];
+                                    return ListTile(
+                                      leading: Image.network(
+                                        book.cover ?? '',
+                                        width: 60,
+                                        height: 80,
+                                      ),
+                                      title: Text(
+                                        '${book.title}',
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.blueAccent),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      subtitle: Column(
+                                        children: [
+                                          Text(
+                                              'Quantity: ${book.price} x ${book.qty}'),
+                                          Text('Author: ${book.author}')
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
+                                const SizedBox(height: 8.0),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
+                );
+              },
+            ),
           ),
         ));
   }
